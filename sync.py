a65a8781ad360b39f5080e81a5ce804463a5394a
xJunko <fireredz69420@gmail.com>
2022-12-04 12:55:44 +0800

    All: Wrote a simple script for this shit

---
+++ sync.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import shutil
+
+# Home
+HOME: Path = Path("/home/junko/")
+CONFIG: Path = HOME / ".config"
+REPO: Path = Path.cwd()
+
+# Files to sync
+FILES_TO_SYNC: dict[Path, Path] = {
+    # ~/
+    HOME / ".zshrc": REPO / "root",
+
+    # .config/
+    CONFIG / "bspwm" / "bspwmrc": REPO / "config" / "bspwm",
+    CONFIG / "sxhkd" / "sxhkdrc": REPO / "config" / "sxhkd",
+    CONFIG / "dunst" / "dunstrc": REPO / "config" / "dunst",
+    CONFIG / "rofi": REPO / "config" / "rofi"
+}
+
+
+def main() -> int:
+    # Delete for redudancy crap
+    for folder in ["config", "root"]:
+        shutil.rmtree(REPO / folder, ignore_errors=True)
+
+    for source, destination in FILES_TO_SYNC.items():
+        # File
+        if not source.is_dir():
+            destination.mkdir(exist_ok=True, parents=True)    
+            shutil.copy(source, destination / source.name)
+
+        # Whole folder
+        if source.is_dir():
+            # destination.mkdir(exist_ok=True, parents=True)
+            shutil.copytree(source, destination)
+            
+        print(f"Syncing: {source} -> {destination}")
+
+    return 0
+
+if __name__ == "__main__":
+    raise SystemExit(main())
